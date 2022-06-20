63476a2af3ffedbb07cdc6f458959154cdfd5e74
Mateus Matos <mateus.m.oliveira2303@gmail.com>
2022-06-20 19:49:49 -0300

    finalizado atividade API

--- script.js
+++ script.js
@@ -2,50 +2,6 @@ let card1 = document.querySelector("#card1")
 let btnAdicionar = document.querySelector("#mostrarInfo")
 let btnRemover = document.querySelector("#removerInfo")
 
-async function carregarDados(){
-    const url = "https://swapi.dev/api/starships/"
-    try {
-        let resultado = await fetch(url)
-        const dados = await resultado.json()
-        console.log(dados.results)
-        for(elementos of dados.results){
-
-            const div1 = document.createElement("div")
-            div1.classList.add("row")
-            div1.classList.add("row-cols-6")
-            div1.classList.add("row-cols-md-1")
-            div1.classList.add("g-4")
-
-            const div2 = document.createElement("div")
-            div2.classList.add("col-6")
-
-            const div3 = document.createElement("div")
-            div3.classList.add("card")
-            div3.classList.add("card-body")
-        
-            const cardTitulo = document.createElement("h5")
-            cardTitulo.classList.add("card-title")
-            cardTitulo.classList.add("text-center")
-            cardTitulo.textContent = `${elementos.name}`
-
-            const cardTexto = document.createElement("p")
-            cardTexto.classList.add("card-text")
-            cardTexto.classList.add("text-center")
-            cardTexto.textContent = `${elementos.model}`
-
-            div1.appendChild(div2)
-            div2.appendChild(div3)
-            div3.appendChild(cardTitulo)
-            div3.appendChild(cardTexto)
-            card1.appendChild(div1)
-
-        }
-    } catch (error) {
-        console.log("o seguinte erro ocorreu: ", error)
-    }
-}
-
-//carregarDados()
 
 async function mostrarDados(){
     const url = "https://swapi.dev/api/starships/"
@@ -54,8 +10,8 @@ async function mostrarDados(){
             const dados = await resultado.json()
             //console.log(dados.results)
             //console.log(cont)
-            let cont = dados.results[0]
-            cont++
+            let elementos = dados.results
+            
             const div1 = document.createElement("div")
             div1.classList.add("row")
             div1.classList.add("row-cols-6")
@@ -68,16 +24,17 @@ async function mostrarDados(){
             const div3 = document.createElement("div")
             div3.classList.add("card")
             div3.classList.add("card-body")
+            div3.classList.add("opacity-75")
         
             const cardTitulo = document.createElement("h5")
             cardTitulo.classList.add("card-title")
             cardTitulo.classList.add("text-center")
-            cardTitulo.textContent = `${cont.name}`
+            cardTitulo.textContent = `Nome: ${elementos[x].name}`
 
             const cardTexto = document.createElement("p")
             cardTexto.classList.add("card-text")
             cardTexto.classList.add("text-center")
-            cardTexto.textContent = `${cont.model}`
+            cardTexto.textContent = `Modelo: ${elementos[x].model}`
 
             div1.appendChild(div2)
             div2.appendChild(div3)
@@ -90,43 +47,20 @@ async function mostrarDados(){
     }
 }
 
-async function removerDados(){
-    const url = "https://swapi.dev/api/starships/"
-    try {
-        let resultado = await fetch(url)
-        const dados = await resultado.json()
-        let cont = dados.results
-        cont--
-                const div1 = document.createElement("div")
-                    div1.classList.remove("row")
-                    div1.classList.remove("row-cols-6")
-                    div1.classList.remove("row-cols-md-1")
-                    div1.classList.remove("g-4")
-
-                    const div2 = document.createElement("div")
-                    div2.classList.remove("col-6")
-
-                    const div3 = document.createElement("div")
-                    div3.classList.remove("card")
-                    div3.classList.remove("card-body")
-        
-                    const cardTitulo = document.createElement("h5")
-                    cardTitulo.classList.remove("card-title")
-                    cardTitulo.classList.remove("text-center")
-                    cardTitulo.textContent = `${elementos.name}`
-
-                    const cardTexto = document.createElement("p")
-                    cardTexto.classList.remove("card-text")
-                    cardTexto.classList.remove("text-center")
-                    cardTexto.textContent = `${elementos.model}`
-        
-        
-     } catch (error) {
-        console.log(error)
+ function removerDados(){
+    if(card1.children.length>0){
+        card1.removeChild(card1.lastElementChild)
     }
 }
 
-btnAdicionar.addEventListener("click", mostrarDados)
+let x = -1
 
-btnRemover.addEventListener("click", removerDados)
+btnAdicionar.addEventListener("click", ()=>{
+    x++
+    mostrarDados()
+})
+
+btnRemover.addEventListener("click", ()=>{
+    removerDados()
+})
 
